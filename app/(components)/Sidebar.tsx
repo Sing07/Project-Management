07c7bc36466789a6c228545c26363cbb7ba0fb2b
SingT <61646543+Sing07@users.noreply.github.com>
2024-12-30 22:08:46 -0800

    sidebar down opens and closes

--- app/(components)/Sidebar.tsx
+++ app/(components)/Sidebar.tsx
@@ -1,19 +1,42 @@
 "use client";
 
-import { Briefcase, Home, Icon, LockIcon, LucideIcon, Search, Settings, Settings2, User, Users } from "lucide-react";
+import {
+    AlertCircle,
+    AlertOctagon,
+    AlertTriangle,
+    Briefcase,
+    ChevronDown,
+    ChevronUp,
+    Home,
+    Icon,
+    Layers3,
+    LockIcon,
+    LucideIcon,
+    Search,
+    Settings,
+    Settings2,
+    ShieldAlert,
+    User,
+    Users,
+    X,
+} from "lucide-react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../redux";
 import Link from "next/link";
 import { isatty } from "tty";
+import { setIsSidebarCollapsed } from "@/state";
 
 export default function Sidebar() {
     const [showProjects, setShowProjects] = React.useState(true);
     const [showPriority, setShowPriority] = React.useState(true);
+      const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
 
     const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
     transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
+    ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}
   `;
 
     return (
@@ -23,14 +46,22 @@ export default function Sidebar() {
                     <div className="text-xl font-bold text-gray-800 dark:text-white">
                         List
                     </div>
+                    {isSidebarCollapsed ? null : (
+                        <button
+                            className="py-3"
+                            onClick={() => {
+                                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+                            }}
+                        >
+                            <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+                        </button>
+                    )}
                 </div>
                 {/* team */}
                 <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4">
                     <Image src="/logo.png" alt="logo" width={40} height={40} />
                     <div>
-                        <h3 className="text-md font-bold tracking-wide">
-                            Sing TEAM
-                        </h3>
+                        <h3 className="text-md font-bold tracking-wide">Sing TEAM</h3>
                         <div className="mt-1 flex items-start gap-2">
                             <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
                             <p className="text-xs text-gray-500">Private</p>
@@ -40,20 +71,64 @@ export default function Sidebar() {
                 {/* navbar links */}
                 <nav className="z-10 w-full">
                     <SidebarLink icon={Home} label="Home" href="/" />
-                    <SidebarLink
-                        icon={Briefcase}
-                        label="Timeline"
-                        href="/timeline"
-                    />
+                    <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
                     <SidebarLink icon={Search} label="Search" href="/search" />
-                    <SidebarLink
-                        icon={Settings2}
-                        label="Settings"
-                        href="/settings"
-                    />
+                    <SidebarLink icon={Settings2} label="Settings" href="/settings" />
                     <SidebarLink icon={User} label="User" href="/user" />
                     <SidebarLink icon={Users} label="Users" href="/users" />
                 </nav>
+                <button
+                    onClick={() => setShowProjects((prev) => !prev)}
+                    className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+                >
+                    <span className="">Projects</span>
+                    {showProjects ? (
+                        <ChevronUp className="h-5 w-5" />
+                    ) : (
+                        <ChevronDown className="h-5 w-5" />
+                    )}
+                </button>
+                {/* Projects List */}
+                <button
+                    onClick={() => setShowPriority((prev) => !prev)}
+                    className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+                >
+                    <span className="">Priority</span>
+                    {showPriority ? (
+                        <ChevronDown className="h-5 w-5" />
+                    ) : (
+                        <ChevronUp className="h-5 w-5" />
+                    )}
+                </button>
+                {showPriority && (
+                    <>
+                        <SidebarLink
+                            icon={AlertCircle}
+                            label="Urgent"
+                            href="/priority/urgent"
+                        />
+                        <SidebarLink
+                            icon={ShieldAlert}
+                            label="High"
+                            href="/priority/high"
+                        />
+                        <SidebarLink
+                            icon={AlertTriangle}
+                            label="Medium"
+                            href="/priority/medium"
+                        />
+                        <SidebarLink
+                            icon={AlertOctagon}
+                            label="Low"
+                            href="/priority/low"
+                        />
+                        <SidebarLink
+                            icon={Layers3}
+                            label="Backlog"
+                            href="/priority/backlog"
+                        />
+                    </>
+                )}
             </div>
         </div>
     );
@@ -68,15 +143,8 @@ interface SidebarLinkProps {
 
 const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
     const pathName = usePathname();
-    const isActive =
-        pathName === href || (pathName === "/" && href === "/dashboard");
-    const screenwidth = window.innerWidth;
+    const isActive = pathName === href || (pathName === "/" && href === "/dashboard");
 
-    const dispatch = useAppDispatch();
-    const isSidebarCollapsed = useAppSelector(
-        (state) => state.global.isSidebarCollapsed
-    );
-    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
     return (
         <Link href={href} className="w-full">
